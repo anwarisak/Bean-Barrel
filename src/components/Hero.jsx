b0c7a55ddd5e759ba6c74cd0f8ca1002810ca54b
anwarisak <anwarisack@gmail.com>
2026-01-29 17:55:26 +0300

    fix font commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,23 @@ import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className="text-white">
-      <div className="max-w-[800px] mt-[-95px] mx-auto h-screen justify-center items-center flex flex-col">
-        <h1 className="uppercase md:text-3xl sm:text-2xl text-4xl my-2 font-bold text-center px-2">
-          More than <span className="text-[#E8D1C5]">coffee</span>  It’s a ritual.
+    <div className="text-[#452829]">
+      <div className="max-w-[800px] mt-[-0px] mx-auto h-screen justify-center items-center flex flex-col">
+        <h1 className="uppercase md:text-5xl sm:text-3xl text-4xl font-bold text-center px-2">
+          More than <span className="text-[#57595B]">coffee</span>  It’s a ritual.
         </h1>
-        <span className="text-[#E8D1C5] md:text-2xl text-3xl my-8 font-thin px-10 text-center">
+        <span className="text-[#57595B] md:text-2xl text-3xl my-5 font-thin px-10 text-center">
           Small-batch roasted beans, crafted by baristas who actually care what
           ends up in your cup.
         </span>
         <ReactTyped
           className="uppercase md:text-2xl text-2xl"
           strings={["Espresso", "steamed milk", "thick foam"]}
-          typeSpeed={150}
+          typeSpeed={200}
           backSpeed={160}
           loop
         />
-        <button className="bg-[#E8D1C5] my-8 md:px-20 px-40 py-3 rounded-lg text-[#452829] cursor-pointer hover:bg-[#57595B] hover:text-[#F3E8DF]">
+        <button className="bg-[#E8D1C5] my-10 md:px-20 px-25 py-3 rounded-3xl text-[#452829] cursor-pointer hover:bg-[#57595B] hover:text-[#F3E8DF] uppercase">
           Get Started
         </button>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import { MdRestaurantMenu } from "react-icons/md";
+import { SiCoffeescript } from "react-icons/si";
 
 const Navbar = () => {
   const [open, SetOpen] = useState(false);
@@ -10,38 +11,40 @@ const Navbar = () => {
     SetOpen(!open);
   };
   return (
-    <div className="flex items-center justify-between max-w-[1240px] mx-auto h-24 px-4 lg:px-8">
-      <h1 className="text-2xl text-[#E8D1C5] uppercase">Bean & Barrel</h1>
+    <div className="flex items-center border-b border-gray-300 justify-between max-w-[1240px] mx-auto h-24 px-6 lg:px-12">
+      <SiCoffeescript color='#57595B' size={30} className=""/>
+
+      {/* <h1 className="text-2xl text-[#57595B] uppercase">Bean & Barrel</h1> */}
       <ul className="hidden md:flex uppercase cursor-pointer">
-        <li className="p-4 text-[#E8D1C5]">Home</li>
-        <li className="p-4 text-[#E8D1C5]">Menu</li>
-        <li className="p-4 text-[#E8D1C5]">About</li>
-        <li className="p-4 text-[#E8D1C5]">Contact</li>
+        <li className="p-4 text-[#57595B]">Home</li>
+        <li className="p-4 text-[#57595B]">Menu</li>
+        <li className="p-4 text-[#57595B]">About</li>
+        <li className="p-4 text-[#57595B]">Contact</li>
       </ul>
       <div onClick={changemenu} className="block md:hidden">
         {open ? (
-          <MdRestaurantMenu color="white" size={25} />
+          <MdRestaurantMenu color="#452829" size={25} />
         ) : (
-          <FiMenu color="white" size={25} />
+          <FiMenu color="#452829" size={25} />
         )}
       </div>
       <div
         className={`
-          fixed top-0 left-0 h-full w-[50%] bg-[#000300] border-r border-gray-800 transform transition-transform ease-in-out duration-300
+          fixed top-0 left-0 h-full w-[70%] bg-[#000300] border-r border-gray-800 transform transition-transform ease-in-out duration-300
           ${open ? "translate-x-0" : "-translate-x-full"}
         `}
       >
         <ul className="w-full uppercase mt-20 cursor-pointer">
-          <li className="py-2 px-4 text-[#E8D1C5] border-b border-gray-400">
+          <li className="py-2 px-4 text-[#57595B] border-b border-gray-400">
             Home
           </li>
-          <li className="py-2 px-4 text-[#E8D1C5] border-b border-gray-400">
+          <li className="py-2 px-4 text-[#57595B] border-b border-gray-400">
             Menu
           </li>
-          <li className="py-2 px-4 text-[#E8D1C5] border-b border-gray-400">
+          <li className="py-2 px-4 text-[#57595B] border-b border-gray-400">
             About
           </li>
-          <li className="py-2 px-4 text-[#E8D1C5]">Contact</li>
+          <li className="py-2 px-4 text-[#57595B]">Contact</li>
         </ul>
       </div>
     </div>
